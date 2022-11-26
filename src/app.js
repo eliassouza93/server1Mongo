c4b431430fde8ba5a50fd43f1ff4f0ad294ad8bf
Elias de Souza <elias99-souza@hotmail.com>
2022-11-26 10:36:11 -0300

    AppMongoDB

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import db from './config/dbConect.js'
+import routes from './routes/index.js'
+
+
 
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
@@ -12,13 +15,12 @@ const app = express();
 
 app.use(express.json())
 
+routes(app)
+
 const livros = [
     { id: 1, 'titulo': 'Senhor dos Aneis' },
     { id: 2, 'titulo': 'O Hobbit' }
 ]
-app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node')
-})
  
 app.get('/livros/:id', (req, res) => {
     let index = buscaLivro(req.params.id)
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -1,3 +1,10 @@
 import express from "express";
 import LivroController from "../controllers/livrosControler.js";
 
+const router = express.Router()
+
+router
+    .get('livros' , LivroController.listarLivros)
+
+    export default router;
+    
